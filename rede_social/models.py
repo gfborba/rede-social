a08e0a4cf24857b838b4914c409250ca89d2d6a9
Guilherme Borba <guifborba@hotmail.com>
2025-05-03 20:02:31 -0300

    adicionando imagens nos posts

--- rede_social/models.py
+++ rede_social/models.py
@@ -5,6 +5,7 @@ class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     content = models.TextField()
+    image = models.ImageField(upload_to='posts/%Y/%m/%d/', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
