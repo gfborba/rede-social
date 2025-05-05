fd01b806046cfac941f52cf6926ca2a4b8690398
Guilherme Borba <guifborba@hotmail.com>
2025-05-05 19:26:14 -0300

    friendship_list

--- rede_social/views.py
+++ rede_social/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-from .models import Post
+from .models import Post, Friendship
 
 def index(request):
     posts = Post.objects.all().order_by('-id')
-    return render(request, 'pages/index.html', {'posts':posts})
+
+    friendship = Friendship.objects.filter(user=request.user).first()
+    friends = friendship.friends.all()
+
+    return render(request, 'pages/index.html', {'posts':posts,'friends': friends})
+
